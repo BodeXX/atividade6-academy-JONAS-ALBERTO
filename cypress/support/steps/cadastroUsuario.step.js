2a9457817dffc70e33ebde16779de3258c1c6008
Iury Oliveira <iuryoliveirasx@gmail.com>
2024-04-30 19:14:52 -0300

    utilizando alias para salver o email gerado pelo faker

--- cypress/support/steps/cadastroUsuario.step.js
+++ cypress/support/steps/cadastroUsuario.step.js
@@ -3,8 +3,6 @@ import { faker } from '@faker-js/faker';
 import CadastroPage from '../pages/cadastro.page';
 const paginaCadastro = new CadastroPage();
 
-var novoEmail;
-
 Given('que acessei a funcionalidade de cadastro', function () {
   cy.visit('./app/index.html');
 });
@@ -14,7 +12,8 @@ When('informar um novo nome', function () {
 });
 
 When('informar um novo e-mail', function () {
-  novoEmail = faker.internet.email();
+  var novoEmail = faker.internet.email();
+  cy.wrap(novoEmail).as('emailFaker');
   paginaCadastro.typeEmail(novoEmail);
 });
 
@@ -24,7 +23,9 @@ When('confirmar a operação', function () {
 });
 
 Then('o usuário será registrado na lista', function () {
-  paginaCadastro.getListaUsuarios().should('contain', novoEmail);
+  cy.get('@emailFaker').then((email) => {
+    paginaCadastro.getListaUsuarios().should('contain', email);
+  });
 });
 
 Then('o usuário não será registrado na lista', function () {
